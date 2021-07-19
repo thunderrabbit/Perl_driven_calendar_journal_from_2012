fce8db1acacfd9fa679c63c6f0941811b9a8da16
nooj <nooj@users.noreply.github.com>
2021-07-19 13:00:38 -0500

    fixed small syntax change required for new perl

--- cgi-bin/preformatted_journal_index_writer.pl
+++ cgi-bin/preformatted_journal_index_writer.pl
@@ -319,12 +319,12 @@ foreach $yearmonth (sort keys %list_of_days_per_year_month) {
 }
 
 # Write empty blocks for non-existant past / future months and years.
-#foreach my $when qw(prev next) {    <---- this gives a syntax error in Perl 5.26
+foreach my $when (qw(prev next)) {    
     print HTML_OUTPUT "\nNO prev YEAR:\n<pre class='calendar'>\n \n \n \n \n</pre>\n\n";
     print HTML_OUTPUT "\nNO prev MONTH:\n<pre class='calendar'>\n \n \n \n \n \n \n</pre>\n\n";
     print HTML_OUTPUT "\nNO next YEAR:\n<pre class='calendar'>\n \n \n \n \n</pre>\n\n";
     print HTML_OUTPUT "\nNO next MONTH:\n<pre class='calendar'>\n \n \n \n \n \n \n</pre>\n\n";
-#}
+}
 
 # Calculate the first date for this $journal_type
 my $first_year_month = (sort keys %list_of_days_per_year_month)[0];
