2bf8c840478b0c2ffb524626cdfa59aa5b952ab4
Rob Nugen <rob@robnugen.com>
2024-01-13 20:12:48 +0900

    Removed cgi-bin/

--- cgi-bin/unusedsetup_navigation.pl
+++
@@ -1,5 +0,0 @@
-not used anymore
-$NAVIGATION_SOURCE_FILE = "/home/barefoot_rob/setup_journal/navigation_data_source.txt";
-$INDEX_DEFINITION_FILE = "/home/barefoot_rob/setup_journal/navigation_definitions.txt";
-
-
