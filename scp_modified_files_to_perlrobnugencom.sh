43615672efc798fbaac7ba4f56be1d4a43791043
Rob Nugen <rob@robnugen.com>
2025-04-17 11:18:55 +0930

    Easily save local changes to Dreamhost

--- scp_modified_files_to_perlrobnugencom.sh
+++ scp_modified_files_to_perlrobnugencom.sh
@@ -1,8 +1,10 @@
 #/bin/bash
 
-inotifywait --exclude '.git/*' -mr -e close_write . | sed -ue 's/ CLOSE_WRITE,CLOSE //' | xargs -d$'\n' -I% scp  -P 22 -i /home/thunderrabbit/.ssh/perl.robnugen.com % dh_r2ixxd@perl.robnugen.com:/home/dh_r2ixxd/perl.robnugen.com/%
 
-# inotifywait -mre close_write . | \      # m = monitor # r = resurse subdirs # e = these events
-# sed -ue 's/ CLOSE_WRITE,CLOSE //' | \         # removing ' CLOSE_WRITE,CLOSE ' leaves us with exactly the file we need to save
-# xargs -d$'\n' -I% \                # converts the stream into something scp can use
-# scp -P 22 -i /home/thunderrabbit/.ssh/ABs_AB_webserver_2018.pem % ubuntu@52.195.7.199:/var/www/admint.andbeyond.co.jp/current/
+DEST_USER="dh_r2ixxd"
+DEST_HOST="perl.robnugen.com"
+DEST_PATH="/home/$DEST_USER/perl.robnugen.com/"
+SSH_KEY="/home/thunderrabbit/.ssh/perl.robnugen.com"
+
+# This will watch for changes in the source directory and scp them to the destination
+inotifywait --exclude '.git/*' -mr -e close_write . | sed -ue 's/ CLOSE_WRITE,CLOSE //' | xargs -d$'\n' -I% scp  -P 22 -i $SSH_KEY % $DEST_USER@$DEST_HOST:$DEST_PATH%
